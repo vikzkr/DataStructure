c70f11d2ea1e26a82ecbedcad299a2be818022e1
vikas.mishra <vikzkr@gmail.com>
2021-10-02 19:17:29 +0530

    Graph DFS BFS

---
+++ DataAlgo/src/Graph/GraphAdjacentList/GraphNode.java
@@ -0,0 +1,17 @@
+package Graph.GraphAdjacentList;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+  public String name;
+  public int index;
+  public boolean isVisited = false;
+
+  public ArrayList<GraphNode> neighbors = new ArrayList<GraphNode>();
+
+  public GraphNode(String name, int index) {
+    this.name = name;
+    this.index = index;
+  }
+}
+
